530b8e36bc60f1b8d6c6a1e335b02dca2e6eb13a
Adham Moussa <adam.moussa62017@gmail.com>
2018-09-13 06:32:20 +0200

    setup formatting for amount

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import numeral from 'numeral';
 
 const ExpenseItem = (props) => {
   return (
@@ -7,7 +8,7 @@ const ExpenseItem = (props) => {
       <Link to={`/edit/${props.id}`}>
         <h3>Expense: {props.description}</h3>
       </Link>
-      <p>Amount: {props.amount}</p>
+      <p>Amount: {numeral(props.amount).format('$0,0.00')}</p>
       <p>Date: {props.createdAt.format('MMM Do, YYYY')}</p>
     </div>
   );
